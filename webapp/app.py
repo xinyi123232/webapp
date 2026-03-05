69789ac25d95c1ffaaaab57e4ffa0d3bb3f24a3b
xinyi123232 <xinyi_wang@dlsu.edu.ph>
2026-03-05 23:48:06 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -8,6 +8,7 @@ script_dir = Path(__file__).parent
 
 @st.cache_data
 def load_scenario(scenario_path):
+    st.cache_data.clear()
     base_path = script_dir / scenario_path
     with open(base_path / "hex.geojson") as f:
         hex_data = json.load(f)
@@ -219,6 +220,7 @@ with right:
     # ).add_to(m)
 
     # st_folium(m, width=1000, height=700)
+
 
 
 
