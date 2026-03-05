d0cd1dd6ab28d913b3b0b968b69d0e9461264d3b
xinyi123232 <xinyi_wang@dlsu.edu.ph>
2026-03-06 00:18:36 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -94,6 +94,7 @@ with left:
         st.metric("Area Improvement Over Current", f"+{metrics['area_improvement']}%")
         st.metric("Demand Covered", f"{metrics['demand_covered']}%")
         st.metric("Demand Improvement Over Current", f"+{metrics['demand_improvement']}%")
+        st.metric("Improvement", f"+{metrics['demand_coveredaaa']}%")
         
 
     elif mode == "Universal Coverage":
@@ -223,6 +224,7 @@ with right:
     # ).add_to(m)
 
     # st_folium(m, width=1000, height=700)
+
 
 
 
