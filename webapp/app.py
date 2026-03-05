ffb31e8ac046eab23e98b1364a314e5bccdc44d5
xinyi123232 <xinyi_wang@dlsu.edu.ph>
2026-03-05 15:19:08 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,17 +6,7 @@ from streamlit_folium import st_folium
 
 script_dir = Path(__file__).parent
 
-@st.cache_data
-def load_scenario(scenario_path):
-    base_path = script_dir / scenario_path
-    with open(base_path / "hex.geojson") as f:
-        hex_data = json.load(f)
-    with open(base_path / "stations.geojson") as f:
-        station_data = json.load(f)
-    with open(base_path / "metrics.json") as f:
-        metrics = json.load(f)
 
-    return hex_data, station_data, metrics
 
 @st.cache_data
 def load_city_boundaries():
@@ -37,6 +27,18 @@ city_boundaries = load_city_boundaries()
 # station_data = load_stations()
 # metrics = load_metrics()
 
+
+@st.cache_data
+def load_scenario(scenario_path):
+    base_path = script_dir / scenario_path
+    with open(base_path / "hex.geojson") as f:
+        hex_data = json.load(f)
+    with open(base_path / "stations.geojson") as f:
+        station_data = json.load(f)
+    with open(base_path / "metrics.json") as f:
+        metrics = json.load(f)
+
+    return hex_data, station_data, metrics
 
 st.set_page_config(layout="wide")
 
