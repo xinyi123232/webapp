5066a23e9e4c3ef8cf5c20c50423252e8527d57b
xinyi123232 <xinyi_wang@dlsu.edu.ph>
2026-03-06 16:34:55 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -172,7 +172,7 @@ with right:
             marker=folium.Marker(
                 #popup=folium.Popup(html_popup, max_width=250),
                 #tooltip=f"Existing: {row['EVCS Name']}",
-                icon=folium.Icon(color=style_station, icon='bolt', prefix='fa'))
+                icon=folium.Icon(color=style_station(station_data), icon='bolt', prefix='fa'))
         ).add_to(EVCS)
 
         folium.GeoJson(
@@ -240,6 +240,7 @@ with right:
     # ).add_to(m)
 
     # st_folium(m, width=1000, height=700
+
 
 
 
